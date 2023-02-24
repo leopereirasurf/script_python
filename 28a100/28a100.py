929b104f7aef7fb7ef3e0d19ba9303d4f2d96e45
leopereirasurf <digitarr123@gmail.com>
2023-02-24 17:36:34 -0300

    Update 28a100.py

--- 28a100/28a100.py
+++ 28a100/28a100.py
@@ -1,9 +1,12 @@
-from random import randint
+from random import randint 
+from time import sleep
 computador = randint (0,5) #faz comoutador pensar
 print ('-=-' *20)
 print ('vou pensar em um numero entre 0 e 5. tente advinhar...')
 print ('-=-' *20)
 jogador = int (input ('Em que numero eu Pensei?')) #jogador tenta adivinhar
+print ('PROCESSANDO...')
+sleep (4)
 if jogador == computador:
     print ('PARABÉNS vecê è o Grande Campeão')
 else:
