9ab0ecb7c02757c1c1eff3900923315b6395f70c
leopereirasurf <digitarr123@gmail.com>
2023-02-24 18:03:11 -0300

    Update captura01.py

--- captura 01/captura01.py
+++ captura 01/captura01.py
@@ -1,4 +1,6 @@
-nome= input ('seu nome porfavor')
-telefone = input ('seu telefone')
-email= input ('email valido')
-print ('obrigado porfavor verificar seu email')
+velocidade = float (input ('qual é a velocidade Atual do carro?'))
+if velocidade > 50:
+    print ('MULTADO! Você essedeu o limite que de 50km/h')
+    multa = (velocidade-50) * 7
+    print('você deve pagar multa R${:.2f}!'.format(multa))
+print ('tenha um bom dia dirija com segurança')
