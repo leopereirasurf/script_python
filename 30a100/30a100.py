b27f67addb52bfef48e554cc5aac0ff0f12d7078
leopereirasurf <digitarr123@gmail.com>
2023-02-25 17:11:20 -0300

    Update 30a100.py

---
+++ 30a100/30a100.py
@@ -0,0 +1,7 @@
+numero = int (input('me dida umnumero qualquer:'))
+resultado = numero % 2
+if resultado == 0:
+    print('o numero {} é par'.format (numero))
+else:
+    print (' o numero {} é impar'.format (numero))
+    
