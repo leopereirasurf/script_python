3b20b4215341852bb920822e9d38418a0a5e0847
leopereirasurf <digitarr123@gmail.com>
2023-02-25 20:35:08 -0300

    novo

--- captura 01/captura01.py
+++
@@ -1,6 +0,0 @@
-velocidade = float (input ('qual é a velocidade Atual do carro?'))
-if velocidade > 50:
-    print ('MULTADO! Você essedeu o limite que de 50km/h')
-    multa = (velocidade-50) * 7
-    print('você deve pagar multa R${:.2f}!'.format(multa))
-print ('tenha um bom dia dirija com segurança')
